4414d890e9bab8ffe354471286120da3cac560b9
KovalSergej <koval.sergej.2014@gmail.com>
2020-10-12 23:06:46 +0300

    06 first commit

---
+++ task-02.js
@@ -0,0 +1,8 @@
+"use strict";
+import users from "/users.js";
+
+const getUsersWithEyeColor = (users, colour) =>
+  users.filter((user) => user.eyeColor === colour);
+
+console.log(getUsersWithEyeColor(users, "blue"));
+// [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
---
+++ task-10.js
@@ -0,0 +1,13 @@
+"use strict";
+import users from "/users.js";
+
+const getSortedUniqueSkills = (users) =>
+  users
+    .reduce((skills, user) => {
+      skills.push(...user.skills);
+      return skills;
+    }, [])
+    .sort()
+    .filter((skill, index, array) => skill !== array[index + 1]);
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
